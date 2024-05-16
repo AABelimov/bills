dfd2ed08643d28b8fd3aab57c2226aa5ec19a34d
AABelimov <belimov98@gmail.com>
2024-05-16 15:49:06 +0300

    Initial commit

---
+++ src/test/java/ru/aabelimov/bills/BillsApplicationTests.java
@@ -0,0 +1,13 @@
+package ru.aabelimov.bills;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BillsApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
